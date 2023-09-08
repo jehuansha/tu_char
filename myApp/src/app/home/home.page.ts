239e73383542264115d146a4b8427b645bacb38e
TusharRTX <tu.mirwani@duocuc.cl>
2023-09-07 21:05:51 -0300

    V6
    Tushar

--- myApp/src/app/home/home.page.ts
+++ myApp/src/app/home/home.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild,ElementRef} from '@angular/core';
+import { Router } from '@angular/router';
+import { IonAvatar } from '@ionic/angular';
+import type { Animation } from '@ionic/angular';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,31 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonAvatar,{read:ElementRef}) avatar!:ElementRef<HTMLIonAvatarElement>;
+  
 
-  constructor() {}
 
+  private animation!:Animation;
+  constructor(private router: Router,private animationCtrl:AnimationController) { }
+
+
+  ngAfterViewInit() {
+    this.animation = this.animationCtrl.create()
+    .addElement(this.avatar.nativeElement)
+    .duration(5000)
+    .iterations(Infinity)
+    .keyframes([
+      {offset:0, transform:'translateX(0px)',opacity:'1'},
+      {offset:0.25, transform:'translateX(100px)',opacity:'0.2'},
+      {offset:0.50, transform:'translateX(0px)',opacity:'1'},
+      {offset:0.75, transform:'translateX(-100px)',opacity:'0.2'},
+      {offset:1, transform:'translateX(0px)',opacity:'1'},
+    ])
+  }
+
+  playAvatar(){
+    this.animation.play();
+  }
+  
 }
+
